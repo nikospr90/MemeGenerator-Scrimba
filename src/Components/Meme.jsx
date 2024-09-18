df5f66d8104350e9ba0ef640b1815e84e1e387de
nikospr90 <nick_prevenios@hotmail.com>
2024-09-18 23:34:22 +0200

    Implemented Meme component, styled it, work a bit with state to update the images

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,24 +1,35 @@
 import "../Styles/Meme/Meme.css";
+import file from "../memesData";
+import { useState } from "react";
 
 function Meme(){
+
+    const [meme, setMeme] = useState([])
+
+    function getMeme(){
+        const data = file.data.memes
+        let response = data[Math.floor(Math.random() * data.length)]['url'];
+        setMeme(response);
+    }
+
     return (
         <div className="meme">
-            <form  action="">
-                <div className="meme--form">
-                    <div className="inputs">
-                        <div className="input--left">
-                            <label htmlFor="">Top Text</label>
-                            <input type="text" id ="toptext" className="toptext" placeholder="Shut up" />
-                        </div>
-                        <div className="input--right">
-                            <label htmlFor="">Bottom Text</label>
-                            <input type="text" id="bottomtext" className="bottomtext" placeholder="And take my money" />
-                        </div>
+            <div className="meme--form">
+                <div className="inputs">
+                    <div className="input--left">
+                        <label htmlFor="toptext" >Top Text</label>
+                        <input type="text" id ="toptext" className="toptext" placeholder="Shut up" />
                     </div>
-                    <button type="submit">Get a new meme image 🚮</button>
+                    <div className="input--right">
+                        <label htmlFor="bottomtext">Bottom Text</label>
+                        <input type="text" id="bottomtext" className="bottomtext" placeholder="And take my money" />
+                    </div>
                 </div>
-            </form>
+                <button onClick={getMeme} type="submit">Get a new meme image 🚮</button>
+            </div>
+            <img className="meme--container" src={meme} alt="meme image"/>
         </div>
     )
 }
 export default Meme;
+
