a2ca6608046311ff7f8cf2c5136df2323c14d703
nikospr90 <nick_prevenios@hotmail.com>
2024-09-18 17:16:18 +0200

    Creating the Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import trollFace from "../assets/icons/troll-face.png";
+
+function Header(){
+    return(
+        <header className="header">
+            <div className="header--left">
+                <img src={trollFace} alt="" className="header--img" />
+                <h1 className="header--title">Meme Generator</h1>
+            </div>
+            <div className="header--right">
+                <p className="header--description">React Couse - Project 3</p>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
